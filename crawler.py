cb987a41fbb631c6e42d67efee3d84a70ac6b007
Michael Köppl <mko.mko31@gmail.com>
2015-02-10 21:09:46 +0100

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,47 @@
+__author__ = 'mko'
+## 10.02.2015
+## pycrawler
+## 
+
+import os
+from urlparse2 import urlparse1
+import urllib
+from bs4 import BeautifulSoup
+
+def clear(): # method to clear the console
+    os.system(['clear','cls'][os.name == 'nt'])
+
+while True:
+    clear() #clear console                        
+    url = raw_input("Enter the url you want to scrape: ")
+    try:
+        response = urllib.urlopen(url)
+    except BaseException, e:
+        print 'We failed to reach a server.'
+        print 'Reason: ', 'Could not find URL', url
+    else:
+        print 'URL is good!'
+
+        print "Crawling " + url
+
+        urls = [url] # list of urls to scrape
+        visited = [url] # urls already visited
+
+        while len(urls) > 0:
+            try:
+                htmltext = urllib.urlopen(urls[0]).read()
+            except:
+                print urls[0]
+            soup = BeautifulSoup(htmltext)
+            urls.pop(0)
+            print len(urls)
+            for tag in soup.findAll('a', href=True):
+                tag['href'] = urlparse1.urljoin(url, tag['href'])
+                print tag['href']
+                if url in tag['href'] and tag['href'] not in visited: #if the url is from any other website than the one we wanted to search on
+                    urls.append(tag['href'])
+                    visited.append(tag['href'])
+        print visited
+    anwer = raw_input("Do you want to crawl another website? (y/n) ")
+    if anwer == "no":
+        break
